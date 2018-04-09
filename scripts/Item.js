af95bce4b48b55c673db3a7b0c8085db2ca8bcc2
Cam Prier <CSPrier@gmail.com>
2018-04-09 15:15:44 -0700

    addItemToShoppingList validation and push to store.items array

--- scripts/Item.js
+++ scripts/Item.js
@@ -7,10 +7,9 @@ const Item = (function() {
       id: cuid(),
       name: name,
       checked: false
-    }
+    };
   }
   function validateName(name) {
-    // console.log(name === 'apples');
     if (name.length === 0) {
       throw new Error('name does not exist');
     }
